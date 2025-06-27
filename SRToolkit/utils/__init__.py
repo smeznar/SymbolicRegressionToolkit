e2b199e40a5fe7acf6d290914f154acebbe48c06
Sebastian Mežnar <smeznar@gmail.com>
2025-06-27 16:54:40 +0200

    Small update to the util module init file

--- SRToolkit/utils/__init__.py
+++ SRToolkit/utils/__init__.py
@@ -7,7 +7,7 @@ Modules:
     expression_tree: The module containing the expression tree data structure and functions for transforming expressions into trees and back.
     expression_compiler: The module containing functions that transform expressions in the infix notation (represented as lists of tokens) to executable python functions.
     expression_simplifier: The module containing functions that simplify an expression using SymPy
-    expression_generation: The module containing helper functions for generating expressions
+    expression_generator: The module containing helper functions for generating expressions
 
 """
 
@@ -15,6 +15,7 @@ from .expression_tree import Node, tokens_to_tree, is_float
 from .symbol_library import SymbolLibrary
 from .expression_compiler import tree_to_function_rec, expr_to_executable_function, expr_to_error_function
 from .expression_simplifier import simplify
+from .expression_generator import generate_from_pcfg, create_generic_pcfg
 
 __all__ = ["SymbolLibrary", "Node", "tokens_to_tree", "is_float", "tree_to_function_rec", "expr_to_executable_function",
-           "expr_to_error_function", "simplify"]
+           "expr_to_error_function", "simplify", "generate_from_pcfg", "create_generic_pcfg"]
